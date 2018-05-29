7cf098634e5455e875b09e85581506953cb46393
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2018-05-29 16:45:32 +0530

    Fixes redirection code.

--- src/js/card.jsx
+++ src/js/card.jsx
@@ -24,11 +24,21 @@ export default class toCoverImage extends React.Component {
       stateVar.siteConfigs = this.props.siteConfigs;
     }
 
+    this.handleClick = this.handleClick.bind(this);
     this.state = stateVar;
   }
 
   exportData() {
     return this.props.selector.getBoundingClientRect();
+  }
+
+  handleClick() {
+    if (this.state.dataJSON.data.tab) {
+      var win = window.open(this.state.dataJSON.data.link, '_blank');
+      win.focus();
+    } else {
+      window.open(this.state.dataJSON.data.link, '_top');
+    }
   }
 
   componentDidMount() {
@@ -106,7 +116,7 @@ export default class toCoverImage extends React.Component {
       case "col3":
         return "focus-area-col3";
       default:
-        return "";        
+        return "";
     }
   }
 
@@ -125,10 +135,10 @@ export default class toCoverImage extends React.Component {
       img = new Image();
     if(url2 && (this.props.mode !='col16' && this.props.mode !='col7' && this.props.mode !="col4" && this.props.mode != 'col3') ){
       image = url2;
-    }  
+    }
     else if(url3 && (this.props.mode !='col16' && this.props.mode !='col7' && this.props.mode !="col4") ){
       image = url3;
-    }  
+    }
     else if(url4 && (this.props.mode !='col16' && this.props.mode !='col7') ){
       image = url4;
     }
@@ -145,20 +155,20 @@ export default class toCoverImage extends React.Component {
 
       height = img.height;
       width = img.width;
-      
+
 
       let cont = document.getElementsByClassName('protograph-card')[0],
         card = cont.getBoundingClientRect(),
         rwidth = width;
-        // processedHeight = this.getHeight(responseImage.target.naturalHeight);  
+        // processedHeight = this.getHeight(responseImage.target.naturalHeight);
       if(width > card.width){
         cont.style.height = (this.props.mode == 'col16') ? "430px" : "250px";
-        
-        
+
+
       }else{
         cont.style.height = (this.props.mode == 'col16') ? "430px" : "250px";
-        
-        
+
+
       }
 
 
@@ -182,7 +192,7 @@ export default class toCoverImage extends React.Component {
        <div className="protograph-toImage-image-container">
          {/* <div className={this.getfocus(this.props.mode)}>Area of focus</div> */}
          <img src={image} alt={data.title} style={style} className='protograph-toImage-image' />
-           
+
        </div>
     );
   }
@@ -223,7 +233,7 @@ export default class toCoverImage extends React.Component {
       return (
         <div id="protograph-div" className={`protograph-laptop-mode ${this.props.mode}`} style={style}>
           <div style={{ padding: 0 }} className={`protograph-card ${this.props.mode}`} style={style}>
-            <a href={data.link} target={`${(data.tab)? '_blank':'_self'}`}><div className="protograph-toImage-image-container">{this.renderImage()}</div></a>
+            <div className="protograph-toImage-image-container" onClick={data.link ? this.handleClick : undefined}>{this.renderImage()}</div>
           </div>
           {this.renderCredits()}
         </div>
